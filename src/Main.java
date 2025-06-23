c60832f14f4727662042bb488e7d8bbef3b8b7ea
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-23 13:48:16 +0900

    Annotation_Reflection @Deprecated &javaDoc

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,9 @@
-class Animal {
-    public void sound() {
-        System.out.println("Animal makes a sound.");
-    }
-}
-
-class Dog extends Animal {
-    @Override
-    public void sound() {
-        System.out.println("Dog barks.");
-    }
-}
-
-
 public class Main {
     public static void main(String[] args) {
-        Animal dog = new Dog();
-         dog.sound();
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.add(1,2));
+        System.out.println(calculator.addNumber(2,4));
     }
 }
+
